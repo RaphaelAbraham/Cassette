4c4e1393cd5e59280ec0c43e9b70bce1c00a066d
RaphaelAbraham <raphi3a@gmail.com>
2023-05-23 16:38:36 +0200

    modif quotidienne

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
+const test = document.querySelector(".test");
+const main = document.querySelector("main");
 
+test.style.display = "none";
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   nav.classList.toggle("active");
+  main.classList.toggle("blur");
 });
 
 document.body.addEventListener("keypress", (e) => {
   if (e.key == "m") {
+    main.classList.toggle("blur");
     hamburger.classList.toggle("active");
     nav.classList.toggle("active");
   }
 });
+
+main.addEventListener("click", () => {
+  main.classList.remove("blur");
+  hamburger.classList.remove("active");
+  nav.classList.remove("active");
+});
+  
