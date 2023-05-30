4f922799027afeb35ef5d94ba7fdb5a11b7be08e
RaphaelAbraham <raphi3a@gmail.com>
2023-05-30 17:40:59 +0200

    ajout de about

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 const test = document.querySelector(".test");
 const main = document.querySelector("main");
-const download = document.querySelector(".download");
+const download = document.querySelector(".downloada");
 
 test.style.display = "none";
 hamburger.addEventListener("click", () => {
@@ -27,13 +27,16 @@ main.addEventListener("click", () => {
 
 download.addEventListener("click", () => {
   const notification = document.createElement("div");
-  notification.textContent = "Notification";
+  notification.textContent = "Powerpoint téléchargé";
   notification.style.position = "fixed";
-  notification.style.top = "10px";
+  notification.style.bottom = "10px";
   notification.style.right = "10px";
+  notification.style.borderRadius = "3px";
+  notification.style.padding = "3px";
+  notification.style.border = "1px solid white";
   document.body.appendChild(notification);
 
   setTimeout(() => {
     notification.remove();
-  }, 2000);
+  }, 3000);
 });
