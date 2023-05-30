5acc56bc7ac65af2b11ced62755aa79a72cd6ae5
RaphaelAbraham <raphi3a@gmail.com>
2023-05-30 20:19:06 +0200

    ajout de la page ressources, et ajout de history

---
+++ ressources.js
@@ -0,0 +1,16 @@
+const test = document.querySelector(".test");
+test.style.display = "none";
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
+document.body.addEventListener("keypress", (e) => {
+  if (e.key == "m") {
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("active");
+  }
+});
